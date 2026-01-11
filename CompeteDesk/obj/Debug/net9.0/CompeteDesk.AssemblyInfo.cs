8501fc4aa2612ae6591409e3e8c40d39e2c3c180
DJ Idorot <ido20002@byui.edu>
2026-01-11 15:09:41 +0800

    War Room

--- CompeteDesk/obj/Debug/net9.0/CompeteDesk.AssemblyInfo.cs
+++ CompeteDesk/obj/Debug/net9.0/CompeteDesk.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompeteDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+557d314b3f226a91ef54eff426d1208576b844c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0827d014259a584fea47fe41f88d7702865e4d91")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompeteDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompeteDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
